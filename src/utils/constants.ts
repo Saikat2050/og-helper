dda3803a1e7c315c57ec27de34ba9a17225fd506
Saikat Santra <38371014+Saikat2050@users.noreply.github.com>
2026-07-01 23:16:47 +0530

    Add new email template type to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,12 @@ export const EMAIL_TEMPLATE_TYPE = {
 		fileName: 'mfm_email.ejs',
 		subject: '🎯 Hot Lead Incoming – Details Inside!',
 		email: 'mfmdelhi24@gmail.com'
+	},
+	2: {
+		fileName: 'thrive_nest_email.ejs',
+		subject: '🎯 Hot Lead Incoming – Details Inside!',
+		email: 'saikatsantra2050@gmail.com'
 	}
 }
 
-export const ALLOWED_EMAIL_TEMPLATE_TYPES = [1]
+export const ALLOWED_EMAIL_TEMPLATE_TYPES = [1, 2]
