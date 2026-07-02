510da75b7af0d6aff7e990495aa5f5fe18f3a8dc
Saikat Santra <38371014+Saikat2050@users.noreply.github.com>
2026-07-02 09:49:42 +0530

    Add new email template type to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,7 +8,12 @@ export const EMAIL_TEMPLATE_TYPE = {
 		fileName: 'thrive_nest_email.ejs',
 		subject: '🎯 Hot Lead Incoming – Details Inside!',
 		email: 'soumas9277@gmail.com'
+	},
+	3: {
+		fileName: 'leads_thrive_nest_email.ejs',
+		subject: '💼 New Website Inquiry | Thrive Nest Labs',
+		email: 'soumas9277@gmail.com'
 	}
 }
 
-export const ALLOWED_EMAIL_TEMPLATE_TYPES = [1, 2]
+export const ALLOWED_EMAIL_TEMPLATE_TYPES = [1, 2, 3]
